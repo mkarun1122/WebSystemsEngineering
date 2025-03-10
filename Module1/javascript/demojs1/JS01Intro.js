6e2cd6d83160c5b07328fd5f5be20ee83daa9a79
M ArunKumar <mkarun1122@gmail.com>
2025-03-10 11:11:01 +0530

    Added demo samples for Module1- clases

---
+++ Module1/javascript/demojs1/JS01Intro.js
@@ -0,0 +1,24 @@
+/*
+JavaScript is a scripting language and it can be included in the HTML using
+the <script> tag which can hold the direct JS code embedded within HTML
+either in the head or the body section
+<script>
+function myJSFunction() {
+document.getElementById("name").innerHTML = "JavaScript";
+}
+</script>
+
+
+
+It can also be written as a separate external file and included as a source in
+head or body as shown as follows:
+
+<script src="myJScript.js"></script>
+*/
+
+function myJSFunctionExt() {
+    document.getElementById("name").innerHTML = " This text calling from JavaScript external file Method";
+    }
+
+
+
