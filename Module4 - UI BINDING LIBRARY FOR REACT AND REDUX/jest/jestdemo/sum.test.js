926b979f1d873a1caaea2887985efc6f54d0b522
Mr M Arun <mkarun1122@gmail.com>
2025-03-27 13:37:33 +0530

    modified files

--- Module4 - UI BINDING LIBRARY FOR REACT AND REDUX/jest/jestdemo/sum.test.js
+++ Module4 - UI BINDING LIBRARY FOR REACT AND REDUX/jest/jestdemo/sum.test.js
@@ -14,13 +14,7 @@ test('two plus two is four', () => {
     expect(data).toEqual({one: 1, two: 2});
   });
 
-  test('adding positive numbers is not zero', () => {
-    for (let a = 1; a < 10; a++) {
-      for (let b = 1; b < 10; b++) {
-        expect(a + b).not.toBe(0);
-      }
-    }
-  });
+
 
   test('null', () => {
     const n = null;
